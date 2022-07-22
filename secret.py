d4aed19f6a9a89dac3da592da0af4cc2ad7c6002
scmpamekasan <109006899+scmpamekasan@users.noreply.github.com>
2022-07-22 13:52:51 +0700

    Update secret.py

--- secret.py
+++ secret.py
@@ -14,8 +14,8 @@ import os
 
 # Everything is accessible via the st.secrets dict:
 
-st.write("DB username:", st.secrets["db_username"])
-st.write("DB password:", st.secrets["db_password"])
+###st.write("DB username:", st.secrets["db_username"])
+###st.write("DB password:", st.secrets["db_password"])
 st.write("My cool secrets:", st.secrets["my_cool_secrets"]["things_i_like"])
 
 # And the root-level secrets are also accessible as environment variables:
