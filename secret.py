7a2bba4908f71fac0ecda4ba11c76f7dbf6bd9d3
scmpamekasan <109006899+scmpamekasan@users.noreply.github.com>
2022-07-22 14:13:10 +0700

    Update secret.py

--- secret.py
+++ secret.py
@@ -24,7 +24,6 @@ st.write(
 )
 
 # Verbose version
-my_db.connect(username=st.secrets.db_credentials.username, password=st.secrets.db_credentials.password)
 
 # Far more compact version!
 my_db.connect(**st.secrets.db_credentials)
