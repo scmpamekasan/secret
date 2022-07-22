ba1835b7d12c87a41d04a41c379589558240ee5a
scmpamekasan <109006899+scmpamekasan@users.noreply.github.com>
2022-07-22 11:50:20 +0700

    Create secret.py

---
+++ secret.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+
+# Everything in this section will be available as an environment variable
+db_username = "Pamekasan"
+db_password = "12345"
+
+# You can also add other sections if you like.
+# The contents of sections as shown below will not become environment variables,
+# but they'll be easily accessible from within Streamlit anyway as we show
+# later in this doc.
+[my_cool_secrets]
+things_i_like = ["Streamlit", "Python"]
