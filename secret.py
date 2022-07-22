0bdd15f5b5cf25b8831aec08f3833323a18255bc
scmpamekasan <109006899+scmpamekasan@users.noreply.github.com>
2022-07-22 11:53:19 +0700

    Update secret.py

--- secret.py
+++ secret.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import os
+
 
 
 # Everything in this section will be available as an environment variable
@@ -11,3 +13,23 @@ db_password = "12345"
 # later in this doc.
 [my_cool_secrets]
 things_i_like = ["Streamlit", "Python"]
+
+# Everything is accessible via the st.secrets dict:
+
+st.write("DB username:", st.secrets["db_username"])
+st.write("DB password:", st.secrets["db_password"])
+st.write("My cool secrets:", st.secrets["my_cool_secrets"]["things_i_like"])
+
+# And the root-level secrets are also accessible as environment variables:
+
+
+st.write(
+    "Has environment variables been set:",
+    os.environ["db_username"] == st.secrets["db_username"],
+)
+
+# Verbose version
+my_db.connect(username=st.secrets.db_credentials.username, password=st.secrets.db_credentials.password)
+
+# Far more compact version!
+my_db.connect(**st.secrets.db_credentials)
